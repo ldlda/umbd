16730b83efa2da2915797130723f53defcca7570
lda <lda@ldlda.com>
2025-09-13 02:26:41 +0700

    ok

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.kt
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.kt
@@ -2,14 +2,24 @@ package vn.edu.usth.weather
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class WeatherActivity : AppCompatActivity() {
     val TAG = javaClass.simpleName
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         Log.i(TAG, "onCreate: triggered")
         val fragment = ForecastFragment()
 

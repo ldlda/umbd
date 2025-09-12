0ebc23f9e1124ad849674ec85c1d25e52ce4dd53
lda <lda@ldlda.com>
2025-09-13 04:08:20 +0700

    lowkey this is why people like react ngl

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.kt
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.kt
@@ -5,6 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,6 +22,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class ForecastFragment : Fragment() {
     val TAG = "ForecastFragment"
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,9 +40,26 @@ class ForecastFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         Log.i(TAG, "onCreateView: triggered")
-        return inflater.inflate(R.layout.fragment_forecast, container, false).apply { // kotlin goated for this lowk
-            setBackgroundColor(listOf(0x200000FF, 0x2000FF00, 0x20FF0000).random()) // why here
+//        return inflater.inflate(R.layout.fragment_forecast, container, false).apply { // kotlin goated for this lowk
+//            setBackgroundColor(listOf(0x200000FF, 0x2000FF00, 0x20FF0000).random()) // why here
+//        }
+
+        val context = requireContext()
+        val drawable = ContextCompat.getDrawable(context, R.drawable.sunny_with_rain_light)
+        val textview = TextView(context).apply {
+            setText(R.string.text_lda)
+            setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, drawable, null)
         }
+        val linearlayout = LinearLayout(context).apply {
+            setBackgroundColor(listOf(0x200000FF, 0x2000FF00, 0x20FF0000).random())
+            orientation = LinearLayout.VERTICAL
+            layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+            )
+            addView(textview)
+        }
+        return linearlayout
     }
 
     companion object {

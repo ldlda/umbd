f93cb1f95a986d2a7bbd2db8eb5afeb5e8a881cb
lda <lda@ldlda.com>
2025-09-13 02:09:36 +0700

    kotlin lowk goated

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.kt
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.kt
@@ -1,10 +1,12 @@
 package vn.edu.usth.weather
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -16,6 +18,7 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ForecastFragment : Fragment() {
+    val TAG = "ForecastFragment"
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,10 +31,14 @@ class ForecastFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_forecast, container, false)
+        Log.i(TAG, "onCreateView: triggered")
+        return inflater.inflate(R.layout.fragment_forecast, container, false).apply { // kotlin goated for this lowk
+            setBackgroundColor(listOf(0x200000FF, 0x2000FF00, 0x20FF0000).random()) // why here
+        }
     }
 
     companion object {
@@ -44,12 +51,12 @@ class ForecastFragment : Fragment() {
          * @return A new instance of fragment ForecastFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                ForecastFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = ForecastFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_PARAM1, param1)
+                putString(ARG_PARAM2, param2)
+            }
+        }
     }
 }
